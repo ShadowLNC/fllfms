ShadowLNC/fllfms

commit 19b03bb18bc190a2341b6ff0d28b97658cdcec86

Admin site for scoresheets (WIP)
Signature and player/referee selection widgets. Minor model changes to facilitate the above.

scoresheets/_base.py
@@ -1,4 +1,7 @@
+from collections import defaultdict
+
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -37,7 +40,22 @@ class BaseScoresheet(models.Model):
         verbose_name=_("station referee"))
     # BLOB preferred vs file: https://arxiv.org/ftp/cs/papers/0701/0701168.pdf
     # Student signature. (PNG < 50KB0)
-    signature = models.BinaryField(verbose_name=_("team initials"))
+    signature = models.BinaryField(editable=True,
+                                   verbose_name=_("team initials"))
+
+    def clean(self, doraise=True):
+        errs = defaultdict(list)
+
+        if self.pk is not None:
+            old = self.__class__.objects.get(pk=self.pk)
+            if old.signature == self.signature:
+                errs['signature'].append(ValidationError(
+                    _("A new signature is required when updating scores."),
+                    code='required_change'))
+
+        if errs and doraise:
+            raise ValidationError(errs)
+        return errs  # In case we're subclassing.
 
     def save(self, *args, **kwargs):
         self.score = self.calculatescore()

admin.py
@@ -1,10 +1,60 @@
+from base64 import b64decode, b64encode
+
+from django import forms
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.contrib import admin, messages
 from django.contrib.admin.widgets import AutocompleteSelect
 from django.db import models
+from django.db.models import Q
+from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
+# from django.views.generic import RedirectView
 
-from .models import Team, Match, Player
+from .models import Team, Match, Player, Scoresheet
+
+
+class SignatureWidget(forms.Widget):
+    template_name = 'fllfms/signature_widget.html'
+
+    def format_value(self, value):
+        value = value or ""
+        if isinstance(value, str):
+            # Happens if we don't use to_python(), e.g. validation failure.
+            return value
+        return str(b64encode(value or b""), 'ascii')
+
+    @property
+    def media(self):
+        return forms.Media(
+            js=(
+                'fllfms/vendor/signature_pad/signature_pad.umd.min.js',
+                'fllfms/signature_widget.js',
+            ),
+            css={},
+        )
+
+
+class SignatureField(forms.Field):
+    initial = None
+    widget = SignatureWidget
+
+    def to_python(self, value):
+        if not value:
+            return None
+        try:
+            return b64decode(value)
+        except (TypeError, ValueError):
+            raise ValidationError(_("Invalid data format."),
+                                  code='invalid')
+
+    def clean(self, value):
+        value = self.to_python(value)
+        if value is None:
+            raise ValidationError(_("A signature is required."),
+                                  code='required')
+
+        return value
 
 
 class PlayerAdmin(admin.TabularInline):
@@ -205,3 +255,68 @@ class MatchAdmin(admin.ModelAdmin):
     inlines = [
         PlayerAdmin,
     ]
+
+
+@admin.register(Scoresheet)
+class ScoresheetAdmin(admin.ModelAdmin):
+    def imgsignature(self, obj):
+        return format_html('<img src="data:image/png;base64,{}">',
+                           str(b64encode(obj.signature), 'ascii'))
+    imgsignature.short_description = _("team initials")
+
+    def get_fieldsets(self, request, obj=None):
+        # Replace signature with imgsignature if it's going to be readonly.
+        # Kinda cheaty, but admin forms can only output text/booleans.
+        # We'd have to subclass at least 3 classes to do it "properly".
+        # I copied the "readonly form" check from ModelAdmin._changeform_view.
+        signature = 'signature'
+        if obj is not None and not self.has_change_permission(request, obj):
+            signature = 'imgsignature'
+
+        return (
+            (_("Player details"), {
+                'fields': ['player', 'referee']
+            }),
+        ) + Scoresheet.fieldsets + (
+            (_("Sign off"), {
+                'fields': [signature]
+            }),
+        )
+
+    _scorefields = sum(
+        (section[1]['fields'] for section in Scoresheet.fieldsets), [])
+
+    def formfield_for_dbfield(self, db_field, request, **kwargs):
+        db = kwargs.get('using')
+
+        if db_field.name == 'player':
+            if 'queryset' not in kwargs:
+                # Specify limiting and especially ordering here, not model.
+                mgr = db_field.remote_field.model._default_manager.using(db)
+                filter = Q(scoresheet__isnull=True,
+                           match__actual__isnull=False)
+
+                # If we're editing existing, show current selection too.
+                obj = request.resolver_match.kwargs.get('object_id', None)
+                if obj is not None:
+                    filter |= Q(scoresheet__pk=obj)
+
+                kwargs['queryset'] = mgr.filter(filter).order_by(
+                    '-match__actual', 'match', 'station')
+
+            # Skip the RelatedFieldWidgetWrapper.
+            return db_field.formfield(**kwargs)
+
+        if db_field.name == 'referee':
+            # Skip the RelatedFieldWidgetWrapper.
+            return db_field.formfield(**kwargs)
+
+        if db_field.name == 'signature':
+            return db_field.formfield(form_class=SignatureField, **kwargs)
+
+        if db_field.name in self._scorefields:
+            # TODO
+            pass
+
+        # Later: filter on referee role for referee field.
+        return super().formfield_for_dbfield(db_field, request, **kwargs)

static/fllfms/signature_widget.js
@@ -1,0 +1,87 @@
+// Requires https://github.com/szimek/signature_pad
+function padsetup() {
+
+    var queryset = document.querySelectorAll(".signature-pad");
+    var padset = [];
+    for (let wrapper of queryset) {
+
+        // Create a SignaturePad object and extend with attributes and methods.
+        var canvas = wrapper.querySelector("canvas");
+        var pad = new SignaturePad(canvas, {
+            penColor: "blue",
+        });
+        pad.wrapper = wrapper;
+        pad.input = wrapper.querySelector("input");
+        pad.overlay = wrapper.querySelector("div").querySelector("div");
+        pad.lock = function() {
+            pad.overlay.style.display = "flex";
+            pad.off();
+            pad.fromDataURL("data:image/png;base64," + pad.input.value);
+        };
+        pad.unlock = function() {
+            pad.overlay.style.display = "none";
+            pad.clear();
+            pad.input.value = "";
+            pad.on();
+        };
+        // "When the pad is updated"
+        pad.onEnd = function() {
+            if (pad.isEmpty()) {
+                pad.input.value = "";
+            } else {
+                // cut data:image/png;base64,
+                pad.input.value = this.toDataURL().substring(22);
+            }
+        };
+        pad._canvasResize = function () {
+            // Adapted from https://github.com/szimek/signature_pad
+            // This is abstracted from resize() as we need it in the setup.
+            pad.dimensions = pad.getDimensions();  // Make sure to update.
+            pad.canvas.width = pad.dimensions.width * pad.dimensions.ratio;
+            pad.canvas.height = pad.dimensions.height * pad.dimensions.ratio;
+            pad.canvas.getContext("2d").scale(
+                pad.dimensions.ratio, pad.dimensions.ratio);
+            pad.clear();  // Otherwise fromDataURL and isEmpty mightn't work.
+        };
+        pad.resize = function () {
+            if (!pad.dimensions.isEqual(pad.getDimensions())) {
+                pad.onEnd();  // Store current value to input.
+                pad._canvasResize();
+                pad.lock();  // _canvasResize clears but this will restore it.
+            }
+        };
+        pad.getDimensions = function () {
+            // Basic set of dimensions for the canvas.
+            return {
+                width: pad.canvas.offsetWidth,
+                height: pad.canvas.offsetHeight,
+                ratio: Math.max(window.devicePixelRatio || 1, 1),
+                isEqual: function (cmp) {
+                    // Basic comparison as objects will never be equal by ref.
+                    return (this.width == cmp.width &&
+                        this.height == cmp.height &&
+                        this.ratio == cmp.ratio);
+                }
+            };
+        };
+
+        // Finish setup and apply loaded data, if present.
+        pad._canvasResize();
+        if (pad.input.value) {
+            pad.lock();  // If previously signed, lock (and load input).
+        }
+        wrapper.querySelector("button").addEventListener("click", pad.unlock);
+
+        // Finally, add pad to list of all signature pads on the page.
+        padset.push(pad);
+    }
+
+    window.addEventListener("resize", function() {
+        for (let pad of padset) {
+            pad.resize();
+        }
+    });
+
+}
+
+window.addEventListener("load", padsetup);

templates/fllfms/signature_widget.html
@@ -1,0 +1,19 @@
+{% load i18n %}
+
+{% if not widget.attrs.disabled %}{# If missing attr, bool(None) == False. #}
+    <!-- The parent div must be slightly larger but still fill maximum, so 26>25.
+    Otherwise it won't auto-expand unless forced to redraw by (un)wrapping. -->
+    <div class="signature-pad" style="display:inline-block; width:26em;">
+        <div style="display:block; box-sizing: border-box; position:relative; top:0; left:0; border-radius: 0.3em; border: 1px solid #ccc; width:100%; max-width:25em; height:10em;">
+            <canvas style="width: 100%; height: 100%;"></canvas>
+            <div style="position:absolute; top:0; left:0; width:100%; height:100%; background-image:linear-gradient(-45deg,rgba(255,255,255,.15) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.15) 50%,rgba(255,255,255,.15) 75%,transparent 75%,transparent); background-size: 1em 1em; font-size: 2em; background-color:#0006; color:#fff; text-align:center; display:none; flex-direction: column; justify-content: center; font-weight:bold;">
+                {% trans "Press clear to redraw" %}</div>
+        </div>
+        <div>
+            <button style="appearance:button; -webkit-appearance:button; -moz-appearance:button;" type="button">{% trans "Clear" %}</button>
+            <input type="hidden" name="{{ widget.name }}" value="{{ widget.value }}" {% include "django/forms/widgets/attrs.html" %}>
+        </div>
+    </div>
+{% else %}
+    <img src="data:image/png;base64,{{ widget.value }}">
+{% endif %}

commit 42d8611f38f680c5f60c3a50fecd351f36dc214f

Updated scoresheet tests (failing since 19b03bb)

scoresheets/_base.py
@@ -52,7 +52,7 @@ class BaseScoresheet(models.Model):
             if old.signature == self.signature:
                 errs['signature'].append(ValidationError(
                     _("A new signature is required when updating scores."),
-                    code='required_change'))
+                    code='signature_must_change'))
 
         if errs and doraise:
             raise ValidationError(errs)

tests/scoresheets/_base.py
@@ -46,7 +46,7 @@ class BaseScoresheetTests(TestCase):
             scoresheet = self.get_base()
         return scoresheet
 
-    def test_defaults(self, **kwargs):
+    def test_defaults(self):
         s = self.with_missions()
         # s.score is not validated and is calculated during save.
         self.assertIsNone(s.score)  # Ensure score does not exist yet.
@@ -54,7 +54,18 @@ class BaseScoresheetTests(TestCase):
         s.save()
         self.assertIsNotNone(s.pk)
         self.assertIsNotNone(s.score)  # Check save() sets score.
+        s.signature = b'4321'  # Set new signature per clean().
         s.full_clean()  # Simulate edit (pk is not None) for clean()/save().
+
+    def test_validate_signature_must_change(self):
+        s = self.with_missions()
+        s.save()
+        self.assertIsNotNone(s.pk)
+        with self.assertRaises(ValidationError):
+            with transaction.atomic():
+                s.full_clean()
+        s.signature = b'4321'  # Change signature and verify error is resolved.
+        s.full_clean()
 
     def test_player_onetoone(self):
         s1 = self.with_missions()
@@ -69,7 +80,7 @@ class BaseScoresheetTests(TestCase):
             with transaction.atomic():
                 s2.save()
 
-    def test_player_delete(self, **kwargs):
+    def test_player_delete(self):
         s = self.with_missions()
         s.save()
         self.assertIsNotNone(s.pk)
@@ -77,7 +88,7 @@ class BaseScoresheetTests(TestCase):
             with transaction.atomic():
                 s.player.delete()
 
-    def test_referee_delete(self, **kwargs):
+    def test_referee_delete(self):
         s = self.with_missions()
         s.save()
         self.assertIsNotNone(s.pk)