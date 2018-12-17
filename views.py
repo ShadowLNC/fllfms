b8296671092bf8e482abc59f144fd3ab9eeb80c2
Scott Stevens <go.scott.mac@gmail.com>
2018-12-17 18:35:32 +1030

    Update rankings view (database query)

--- views.py
+++ views.py
@@ -15,8 +15,14 @@ def rankings(request, tournament):
     annotates = {}
     maxround = Match.objects.filter(tournament=tournament).aggregate(
         maxround=Max('round'))['maxround']
+    # Scores are not available for disqualified teams.
     teamscores = Scoresheet.objects.filter(
-        player__team=OuterRef('pk'), player__surrogate=False)
+        player__team=OuterRef('pk'), player__team__dq=False,
+        player__surrogate=False)
+    # The ranking sort order is best score, second best, etc. but only for
+    # eligible teams, hence dq=False sorts higher (and teamscores removes
+    # scores for disqualified teams, so they all sort equal last).
+    rank_sort = [F('dq').asc()]
 
     # best1, best2, ... for ranking and round1, round2, ... for output.
     for round in range(1, maxround + 1):
@@ -24,13 +30,12 @@ def rankings(request, tournament):
             teamscores.order_by('score')[round - 1:round].values('score'))
         annotates["round{}".format(round)] = Subquery(
             teamscores.filter(player__match__round=round).values('score'))
+        # Update ranking sort order with the nth best score.
+        rank_sort.append(F("best{}".format(round)).desc(nulls_last=True))
 
-    annotates['rank'] = Window(expression=Rank(), order_by=[
-        F("best{}".format(round)).desc() for round in range(1, maxround + 1)])
-
-    # Only rank eligible teams. Add disqualified teams later (no scores).
-    teams = Team.objects.filter(dq=False).annotate(
-        **annotates).order_by('rank', 'number')
+    annotates['rank'] = Window(expression=Rank(), order_by=rank_sort)
+    # Rank all teams (disqualified teams are all equal last, see above).
+    teams = Team.objects.annotate(**annotates).order_by('rank', 'number')
 
     # Patch each object with the scores property for ease of template access.
     def patch(t):
@@ -40,9 +45,6 @@ def rankings(request, tournament):
         t.scores = scores.__get__(t)
         return t
     teams = map(patch, teams)
-
-    # Add disqualified teams as mentioned above (we do it after patching).
-    teams = (*teams, *Team.objects.filter(dq=True).order_by('number'))
 
     context = {
         'teams': teams,
