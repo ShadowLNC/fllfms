1a43f8663e084bb109fe7e2b777862edd8ba18da
Scott Stevens <go.scott.mac@gmail.com>
2019-01-09 21:13:41 +1030

    Begin packaging/deployment toolset

---
+++ packaging/djangoproject/routing.py
@@ -0,0 +1,11 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+import fllfms.routing
+
+application = ProtocolTypeRouter({
+    # http -> Django views is automatically added.
+    'websocket': AllowedHostsOriginValidator(AuthMiddlewareStack(
+        URLRouter(fllfms.routing.websocket_urlpatterns)))
+})
